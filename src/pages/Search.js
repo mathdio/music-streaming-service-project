bf8732e56f3fa41842edfec5e8c4c5121b3904fc
matheus <mathdiogenes@gmail.com>
2022-08-30 20:40:51 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,47 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    searchText: '',
+    disableButton: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const inputMinLimit = 2;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+    if (value.length >= inputMinLimit) {
+      this.setState({
+        disableButton: false,
+      });
+    }
+  };
+
   render() {
+    const { searchText, disableButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            name="searchText"
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.handleChange }
+            value={ searchText }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ disableButton }
+            onClick={ this.handleLogin }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
