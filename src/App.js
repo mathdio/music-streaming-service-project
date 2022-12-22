ca7fa7a5e8630f259c04f997359d647372d6f4e8
matheus <mathdiogenes@gmail.com>
2022-12-22 11:48:16 -0300

    style: Login page's background and form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
+import './reset.css';
 
 class App extends React.Component {
   render() {
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import Loading from './Loading';
 import { createUser } from '../services/userAPI';
+import './Login.css';
 
 class Login extends React.Component {
   state = {
@@ -37,27 +38,30 @@ class Login extends React.Component {
     return (
       savedLoginName ? <Redirect to="/search" />
         : (
-          <div data-testid="page-login">
+          <main data-testid="page-login" className="Login-main-container">
             {loadingLogin ? <Loading />
               : (
-                <form>
+                <form className="Login-form-container">
                   <input
                     name="loginName"
                     type="text"
                     data-testid="login-name-input"
                     onChange={ this.handleChange }
                     value={ loginName }
+                    placeholder="What's your name?"
+                    className="Login-input"
                   />
                   <button
                     type="button"
                     data-testid="login-submit-button"
                     disabled={ disableButton }
                     onClick={ this.handleLogin }
+                    className="Login-button"
                   >
-                    Entrar
+                    ENTER
                   </button>
                 </form>)}
-          </div>
+          </main>
         )
     );
   }
